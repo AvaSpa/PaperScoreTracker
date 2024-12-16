d4ae6d3c13ee87cc05c440612087796df2a911cd
Adrian Cigher <adrian.cigher@gmail.com>
2024-12-16 22:26:56 +0200

    Added custom button

---
+++ PaperScoreTracker/Controls/EncircledButton.xaml.cs
@@ -0,0 +1,33 @@
+using System.Windows.Input;
+
+namespace PaperScoreTracker.Controls;
+
+public partial class EncircledButton : ContentView
+{
+    public static readonly BindableProperty ButtonTextProperty = BindableProperty.Create(nameof(ButtonText), typeof(string), typeof(EncircledButton), string.Empty);
+    public static readonly BindableProperty ClickCommandProperty = BindableProperty.Create(nameof(ClickCommand), typeof(ICommand), typeof(EncircledButton), null);
+    public static readonly BindableProperty ButtonTextFontSizeProperty = BindableProperty.Create(nameof(ButtonTextFontSize), typeof(double), typeof(EncircledButton), 16d);
+
+    public string ButtonText
+    {
+        get => (string)GetValue(ButtonTextProperty);
+        set => SetValue(ButtonTextProperty, value);
+    }
+
+    public ICommand ClickCommand
+    {
+        get => (ICommand)GetValue(ClickCommandProperty);
+        set => SetValue(ClickCommandProperty, value);
+    }
+
+    public double ButtonTextFontSize
+    {
+        get => (double)GetValue(ButtonTextFontSizeProperty);
+        set => SetValue(ButtonTextFontSizeProperty, value);
+    }
+
+    public EncircledButton()
+    {
+        InitializeComponent();
+    }
+}
