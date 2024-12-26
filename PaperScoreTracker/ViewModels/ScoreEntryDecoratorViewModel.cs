2f3a04cc3f588863514a2b1b88b501fd51f9d1d3
Adrian Cigher <adrian.cigher@gmail.com>
2024-12-26 16:30:01 +0200

    Implemented custom number input and tweaked for emulator

---
+++ PaperScoreTracker/ViewModels/ScoreEntryDecoratorViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Core.Models;
+
+namespace PaperScoreTracker.ViewModels;
+
+public partial class ScoreEntryDecoratorViewModel : ObservableObject
+{
+    private ScoreEntry _model;
+
+    [ObservableProperty]
+    private int _incrementValue = 1;
+
+    public int ScoreValue
+    {
+        get => _model.Value;
+        set
+        {
+            var changed = SetProperty(_model.Value, value, _model, (model, value) => model.Value = value);
+
+            if (changed)
+                OnPropertyChanged(nameof(ScoreValue));
+        }
+    }
+
+    public ScoreEntryDecoratorViewModel(ScoreEntry model)
+    {
+        _model = model;
+    }
+
+    [RelayCommand]
+    private void Decrement()
+    {
+        ScoreValue -= IncrementValue;
+    }
+
+    [RelayCommand]
+    private void Increment()
+    {
+        ScoreValue += IncrementValue;
+    }
+}
