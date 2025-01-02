a84889a942ddefc3ccb2827c57381261202584fd
Adrian Cigher <adrian.cigher@gmail.com>
2025-01-02 14:41:13 +0200

    Code written; binding not working

---
+++ PaperScoreTracker/Controls/PaperCheckBox.xaml.cs
@@ -0,0 +1,24 @@
+namespace PaperScoreTracker.Controls;
+
+public partial class PaperCheckBox : ContentView
+{
+    public static readonly BindableProperty IsCheckedProperty = BindableProperty.Create(nameof(IsChecked), typeof(bool), typeof(PaperCheckBox), false);
+    public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(PaperCheckBox), string.Empty);
+
+    public bool IsChecked
+    {
+        get => (bool)GetValue(IsCheckedProperty);
+        set => SetValue(IsCheckedProperty, value);
+    }
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public PaperCheckBox()
+    {
+        InitializeComponent();
+    }
+}
