cedeacfbb2541155c5109d10a5cbbca7152b7eab
Adrian Cigher <adrian.cigher@gmail.com>
2025-01-03 18:22:11 +0200

    Implemented the UI functionality; need to implement the visual

--- PaperScoreTracker/Controls/PaperCheckBox.xaml.cs
+++ PaperScoreTracker/Controls/PaperCheckBox.xaml.cs
@@ -1,14 +1,22 @@
+
+using CommunityToolkit.Mvvm.Input;
+
 namespace PaperScoreTracker.Controls;
 
 public partial class PaperCheckBox : ContentView
 {
-    public static readonly BindableProperty IsCheckedProperty = BindableProperty.Create(nameof(IsChecked), typeof(bool), typeof(PaperCheckBox), false, BindingMode.TwoWay);
+    public static readonly BindableProperty IsCheckedProperty = BindableProperty.Create(nameof(IsChecked), typeof(bool), typeof(PaperCheckBox), false, BindingMode.TwoWay, propertyChanged: OnIsCheckedPropertyChanged);
     public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(PaperCheckBox), string.Empty, BindingMode.TwoWay);
 
     public bool IsChecked
     {
         get => (bool)GetValue(IsCheckedProperty);
-        set => SetValue(IsCheckedProperty, value);
+        set
+        {
+            SetValue(IsCheckedProperty, value);
+
+            ToggleVisual(value);
+        }
     }
 
     public string Text
@@ -17,8 +25,34 @@ public partial class PaperCheckBox : ContentView
         set => SetValue(TextProperty, value);
     }
 
+    public IRelayCommand ToggleStateCommand => new RelayCommand(ToggleCheckedState);
+
     public PaperCheckBox()
     {
         InitializeComponent();
+    }
+
+    private static void OnIsCheckedPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var newIsChecked = (bool)newValue;
+        var oldIsChecked = (bool)oldValue;
+
+        if (newIsChecked == oldIsChecked)
+            return;
+
+        if (bindable is not PaperCheckBox paperCheckBox)
+            return;
+
+        paperCheckBox.IsChecked = newIsChecked;
+    }
+
+    private void ToggleCheckedState()
+    {
+        IsChecked = !IsChecked;
+    }
+
+    private void ToggleVisual(bool isChecked)
+    {
+        CheckBoxButton.Source = ImageSource.FromFile(isChecked ? "encircle.png" : "delete.svg");
     }
 }
