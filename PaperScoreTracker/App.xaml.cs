61b0c6483112d5131db4bea2156e619bdef4ded7
Adrian Cigher <adrian.cigher@gmail.com>
2025-05-28 12:40:25 +0300

    Forcing the app to light theme

--- PaperScoreTracker/App.xaml.cs
+++ PaperScoreTracker/App.xaml.cs
@@ -5,6 +5,8 @@ public partial class App : Microsoft.Maui.Controls.Application
     public App()
     {
         InitializeComponent();
+
+        UserAppTheme = AppTheme.Light;//TODO: for the future add another white ellipsis for encircled button and change the popup background (and all other hardcoded colors)
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
